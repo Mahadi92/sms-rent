2720c5ead5c17ac15ea9fd8bc0125709bc1af1a1
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2022-08-06 02:31:11 +0600

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Avatar = () => <h1>avatar</h1>;
+
+const Navbar = () => {
+  return (
+    <div className="wrapper py-10 flex justify-between items-center">
+      {/* <Link href="/" passHref> */}
+      {/* <Image
+          src="/static/brand/return0-logo.png"
+          width="100"
+          height="60"
+          alt=""
+        /> */}
+      {/* </Link> */}
+      <div className="rounded-full-img">
+        <Image src="/static/images/profile.jpg" width={50} height={50} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import DefaultLayout from "@/components/layouts/DefaultLayout";
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <DefaultLayout>
+      <div>
+        <h1>hello</h1>
+      </div>
+    </DefaultLayout>
+  );
+};
+
+export default Home;
